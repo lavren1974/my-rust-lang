e63252533429296b826b2c3686ae5224bd970670
lavren1974 <lavren1974@gmail.com>
2023-08-09 16:02:25 +0700

    dev/todo_app add

---
+++ dev/todo_app/src/views/to_do/get.rs
@@ -0,0 +1,14 @@
+use crate::state::read_file;
+use actix_web::{web, Responder};
+use serde_json::value::Value;
+use serde_json::Map;
+
+pub async fn get() -> impl Responder {
+    let state: Map<String, Value> = read_file("./state.json");
+    return web::Json(state);
+}
+
+// pub async fn get() -> Map<String, Value> {
+//     let state: Map<String, Value> = read_file("./state.json");
+//     return state;
+// }
